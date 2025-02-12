b3ddfd4ee7347f6eddade9bf445df37dcb162a2d
goajos <jappecremers@gmail.com>
2025-02-12 22:54:32 +0100

    feat: task create, edit, delete

--- src/main.py
+++ src/main.py
@@ -1,12 +1,18 @@
 import flet as ft
 from todo_android.storages import BaseStorage
+from todo_app import TodoApp
 
 storage = BaseStorage()
 
-def main(page: ft.Page):
-    storage.get_session()
-    storage.close()
 
-    page.add(ft.Text(value="Hello, World!"))
+def main(page: ft.Page):
+    page.padding = ft.padding.only(top=50)
+    page.title = 'ToDo App'
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.update()
+
+    todo = TodoApp()
+    page.add(todo)
+
 
 ft.app(main)
