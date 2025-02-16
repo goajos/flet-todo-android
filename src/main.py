1281e15db65a91fae848417a80e9967620da36f2
goajos <jappecremers@gmail.com>
2025-02-16 11:41:30 +0100

    feat: bugged tabs android

--- src/main.py
+++ src/main.py
@@ -8,10 +8,33 @@ def main(page: ft.Page):
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.update()
 
-    todo = TodoApp()
-    page.add(todo)
+    todo1 = TodoApp()
+    todo2 = TodoApp()
+
+    def refresh_tasks(e):
+        tab = e.control.tabs[int(e.data)]
+        tab.content.refresh_tasks()
+        page.update()
+
+    tabs = ft.Tabs(
+        tabs=[
+            ft.Tab(
+                text='To-Do 1',
+                content=todo1,
+            ),
+            ft.Tab(
+                text='To-Do 2',
+                content=todo2,
+            ),
+        ],
+        on_click=refresh_tasks,
+    )
+
+    page.add(tabs)
+
     # load in the persisted tasks
-    todo.refresh_tasks()
+    # todo1.refresh_tasks()
+    # todo2.refresh_tasks()
 
 
 ft.app(main)
